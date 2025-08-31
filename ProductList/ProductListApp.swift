5cece507a8b063702f8540876660eaf2b355f0a7
alejandrovelascoc <cristian.velasco@ingenierosmafur.com>
2025-08-31 13:12:27 -0600

    Initial Commit

---
+++ ProductList/ProductListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListApp.swift
+//  ProductList
+//
+//  Created by DaCodes on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
